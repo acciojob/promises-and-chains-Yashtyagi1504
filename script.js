622ba073b23ea89eabf4c07c4d8e88ac8817662d
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-16 23:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let form = document.querySelector("form");
+let age = document.querySelector("#age");
+let name = document.querySelector("#name");
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+  if (age.value == "" || name.value == "") {
+    alert("Please enter valid details.");
+  } else {
+    let promise = new Promise((resolve, reject) => {
+      if (parseInt(age.value) > 18) {
+        resolve("Welcome, . You can vote.");
+      } else {
+        reject("Oh sorry . You aren't old enough.");
+      }
+    });
+    promise.then((data) => {
+      setTimeout(() => {
+        alert(data);
+      }, 4000);
+    });
+    promise.catch((err) => {
+      setTimeout(() => {
+        alert(err);
+      }, 4000);
+    });
+  }
+	age.value = "";
+	name.value = "";
+});
