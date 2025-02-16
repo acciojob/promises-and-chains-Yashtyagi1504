19a9843a6cb4e79078da28406c6fd0563b3ae104
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-17 00:01:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 let form = document.querySelector("form");
 let age = document.querySelector("#age");
 let name = document.querySelector("#name");
+
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
-  if (age.value == "" || name.value == "") {
+  e.preventDefault();
+
+  let userName = name.value.trim();
+  let userAge = parseInt(age.value, 10);
+
+  if (userName === "" || isNaN(userAge)) {
     alert("Please enter valid details");
-  } else {
-    let promise = new Promise((resolve, reject) => {
-      if (parseInt(age.value) > 18) {
-        resolve(`Welcome, ${name.value}. You can vote.`);
-      } else {
-		  setTimeout(()=>{
-			  reject(`Oh sorry ${name.value}. You aren't old enough.`);
-		  },4000)
-      }
-    });
-    promise.then((data) => {
-      setTimeout(() => {
-        alert(data);
-      }, 4000);
-    });
-    promise.catch((err) => {
-		alert(err)
-    });
+    return;
   }
+
+  let promise = new Promise((resolve, reject) => {
+    if (userAge > 18) {
+      resolve(`Welcome, ${userName}. You can vote.`);
+    } else {
+      reject(`Oh sorry, ${userName}. You aren't old enough.`);
+    }
+  });
+
+  promise
+    .then((message) => {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(message), 4000);
+      });
+    })
+    .then((delayedMessage) => {
+      alert(delayedMessage);
+    })
+    .catch((error) => {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(error), 4000);
+      });
+    })
+    .then((delayedError) => {
+      alert(delayedError);
+    });
 });
